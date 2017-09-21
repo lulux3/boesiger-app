bd8db02a7b5304207b6986a708161b76161a9de0
Luca Tirabassi <luca.tirabassi@cresoft.local>
2017-09-21 16:51:49 +0200

    Trying a new layout approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,6 +9,9 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
+import { CarouselComponent } from './carousel/carousel.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
+import { GooglemapsComponent } from './googlemaps/googlemaps.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,18 @@ import { routing } from './app.routing';
     NavbarComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CarouselComponent,
+    KontaktComponent,
+    GooglemapsComponent
   ],
   imports: [
     BrowserModule,
     AlertModule.forRoot(),
-    routing
+    routing,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAhy49AoTqIrFtm-I8GTg9OIUlZpnrXXgU'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'kontakt', component: KontaktComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
